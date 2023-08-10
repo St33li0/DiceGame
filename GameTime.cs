acfb751faaa2d661671e349f0a6c90734977fffd
St33li0 <jacobsteel9@gmail.com>
2023-08-10 07:33:40 +0100

    Prep for restructuring

---
+++ GameTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    internal class GameTime
+    {
+        int gameTime;
+        bool gameOver = false;
+        public GameTime(int tickIncrement, bool isDead) {
+            gameTime += tickIncrement;
+            gameOver = isDead;
+        }
+    }
+}
